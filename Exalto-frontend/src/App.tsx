2c955c689633c7b82d995d9ae506c522183e8229
graceniyigena34 <graceniyigena34@gmail.com>
2026-08-20 14:47:49 +0200

    added map on contact page

--- Exalto-frontend/src/App.tsx
+++ Exalto-frontend/src/App.tsx
@@ -6,6 +6,7 @@ import Footer from "./components/Footer";
 import Home from "./pages/home";
 import ShopPage from "./pages/shop";
 import AboutPage from "./pages/about";
+import ContactPage from "./pages/contact";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<ShopPage />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
 
       <Footer />
