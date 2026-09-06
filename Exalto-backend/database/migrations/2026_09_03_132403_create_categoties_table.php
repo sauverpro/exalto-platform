2c2290d8d95ca45461f5bd60d058554a5614b6af
justine-neema <j.neema@alustudent.com>
2026-09-06 23:27:43 +0200

    fix: error corrections

--- Exalto-backend/database/migrations/2026_09_03_132403_create_categoties_table.php
+++ Exalto-backend/database/migrations/2026_09_03_132403_create_categoties_table.php
@@ -13,11 +13,11 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
             $table->string('name');
             $table->string('slug');
             $table->string('description');
             $table->boolean('is_active')->default(true);
+            $table->timestamps();
         });
     }
 
